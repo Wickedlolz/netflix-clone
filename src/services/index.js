70f8796461641935a6b5536a47df664d8b4d019c
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-28 19:41:18 +0300

    export services

---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+import * as movieService from './movieService';
+import * as showService from './showService';
+import * as userService from './userService';
+
+export { movieService, showService, userService };
