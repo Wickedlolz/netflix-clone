76e8a2849174995e12871a9193f323f7f9f4e518
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-28 19:41:40 +0300

    add services for like/unlike and add/remove from watch list

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,24 +1,51 @@
+import {
+    doc,
+    updateDoc,
+    arrayUnion,
+    getDoc,
+    arrayRemove,
+} from 'firebase/firestore';
 import { requests } from '../utils/requests';
+import { db } from 'src/firebase.config';
 
-export async function addToWatchlist(accountId, sessionId, movieId) {
-    const response = await fetch(
-        requests.requestAddToWatchlist(accountId, sessionId),
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                media_type: 'movie',
-                media_id: movieId,
-                watchlist: true,
-            }),
-        }
-    );
+export async function addToWatchlist(userEmail, movieData) {
+    const user = await getDoc(doc(db, 'users', userEmail));
+    const isAdded = user.data().watchLater.find((m) => m.id === movieData.id);
+    let added = false;
 
-    const data = await response.json();
+    if (isAdded) {
+        await updateDoc(doc(db, 'users', userEmail), {
+            watchLater: arrayRemove(movieData),
+        });
+        added = false;
+    } else {
+        await updateDoc(doc(db, 'users', userEmail), {
+            watchLater: arrayUnion(movieData),
+        });
+        added = true;
+    }
 
-    return data;
+    return added;
+}
+
+export async function likeUnlike(userEmail, movieId) {
+    const user = await getDoc(doc(db, 'users', userEmail));
+    const isLiked = user.data().likedShows.find((mid) => mid === movieId);
+    let liked = false;
+
+    if (isLiked) {
+        await updateDoc(doc(db, 'users', userEmail), {
+            likedShows: arrayRemove(movieId),
+        });
+        liked = false;
+    } else {
+        await updateDoc(doc(db, 'users', userEmail), {
+            likedShows: arrayUnion(movieId),
+        });
+        liked = true;
+    }
+
+    return liked;
 }
 
 export async function markAsFavourite(accountId, session_id, movieId) {
