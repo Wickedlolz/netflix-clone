ebd067d46364691dad3422c57c32beed23d57638
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-28 16:32:17 +0300

    add sign in with email and pass

--- src/context/FirebaseContext.js
+++ src/context/FirebaseContext.js
@@ -1,4 +1,9 @@
-import { onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import {
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+} from 'firebase/auth';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { auth, googleProvider } from '../firebase.config';
 import { Outlet } from 'react-router-dom';
@@ -16,12 +21,17 @@ export const FirebaseContextProvider = ({ children }) => {
         return () => unsubscribe();
     }, []);
 
+    const signIn = (email, password) =>
+        signInWithEmailAndPassword(auth, email, password);
+
     const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
 
     const logOut = () => signOut(auth);
 
     return (
-        <FirebaseContext.Provider value={{ user, signInWithGoogle, logOut }}>
+        <FirebaseContext.Provider
+            value={{ user, signIn, signInWithGoogle, logOut }}
+        >
             {children ? children : <Outlet />}
         </FirebaseContext.Provider>
     );
