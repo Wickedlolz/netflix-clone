304ef5c92ca26c967dcae0eb433cf2dc86c2c7ed
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-28 18:26:21 +0300

    add after sign up or sign in with google add user to firestore

--- src/context/FirebaseContext.js
+++ src/context/FirebaseContext.js
@@ -1,12 +1,14 @@
 import {
+    createUserWithEmailAndPassword,
     onAuthStateChanged,
     signInWithEmailAndPassword,
     signInWithPopup,
     signOut,
 } from 'firebase/auth';
 import { createContext, useContext, useEffect, useState } from 'react';
-import { auth, googleProvider } from '../firebase.config';
+import { auth, db, googleProvider } from '../firebase.config';
 import { Outlet } from 'react-router-dom';
+import { setDoc, doc } from 'firebase/firestore';
 
 export const FirebaseContext = createContext();
 
@@ -24,13 +26,29 @@ export const FirebaseContextProvider = ({ children }) => {
     const signIn = (email, password) =>
         signInWithEmailAndPassword(auth, email, password);
 
-    const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
+    const signUp = async (email, password) => {
+        await createUserWithEmailAndPassword(auth, email, password);
+        await setDoc(doc(db, 'users', email), {
+            savedShows: [],
+            likedShows: [],
+            watchLater: [],
+        });
+    };
+
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, googleProvider);
+        await setDoc(doc(db, 'users', result.user.email), {
+            savedShows: [],
+            likedShows: [],
+            watchLater: [],
+        });
+    };
 
     const logOut = () => signOut(auth);
 
     return (
         <FirebaseContext.Provider
-            value={{ user, signIn, signInWithGoogle, logOut }}
+            value={{ user, signIn, signInWithGoogle, signUp, logOut }}
         >
             {children ? children : <Outlet />}
         </FirebaseContext.Provider>
