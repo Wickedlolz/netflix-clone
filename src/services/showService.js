05383a801d8e67c298719bcd01f47392d05547b6
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-29 18:04:30 +0300

    add service for add to watchlist and like/unlike

--- src/services/showService.js
+++ src/services/showService.js
@@ -1,22 +1,50 @@
-import { requests } from '../utils/requests';
+import {
+    arrayRemove,
+    arrayUnion,
+    doc,
+    getDoc,
+    updateDoc,
+} from 'firebase/firestore';
+import { db } from 'src/firebase.config';
 
-export async function addToWatchlist(accountId, sessionId, showId) {
-    const response = await fetch(
-        requests.requestAddToWatchlist(accountId, sessionId),
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                media_type: 'tv',
-                media_id: showId,
-                watchlist: true,
-            }),
-        }
-    );
+export async function addToWatchlist(userEmail, showData) {
+    const user = await getDoc(doc(db, 'users', userEmail));
+    const isAdded = user.data().watchLater.find((m) => m.id === showData.id);
 
-    const data = await response.json();
+    if (isAdded) {
+        const updatedList = user
+            .data()
+            .watchLater.filter((m) => m.id !== showData.id);
 
-    return data;
+        await updateDoc(doc(db, 'users', userEmail), {
+            watchLater: updatedList,
+        });
+
+        return false;
+    } else {
+        await updateDoc(doc(db, 'users', userEmail), {
+            watchLater: arrayUnion(showData),
+        });
+
+        return true;
+    }
+}
+
+export async function likeUnlike(userEmail, showId) {
+    const user = await getDoc(doc(db, 'users', userEmail));
+    const isLiked = user.data().likedShows.find((mid) => mid === showId);
+
+    if (isLiked) {
+        await updateDoc(doc(db, 'users', userEmail), {
+            likedShows: arrayRemove(showId),
+        });
+
+        return false;
+    } else {
+        await updateDoc(doc(db, 'users', userEmail), {
+            likedShows: arrayUnion(showId),
+        });
+
+        return true;
+    }
 }
