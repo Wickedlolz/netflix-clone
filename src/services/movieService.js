68813dad3e6e5ebc53bfcb3e76f2c6bd989b0c29
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-09-29 17:36:15 +0300

    remove unnecessary function

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,7 +5,6 @@ import {
     getDoc,
     arrayRemove,
 } from 'firebase/firestore';
-import { requests } from '../utils/requests';
 import { db } from 'src/firebase.config';
 
 export async function addToWatchlist(userEmail, movieData) {
@@ -48,25 +47,4 @@ export async function likeUnlike(userEmail, movieId) {
 
         return true;
     }
-}
-
-export async function markAsFavourite(accountId, session_id, movieId) {
-    const response = await fetch(
-        requests.requestMarkAsFavorite(accountId, session_id),
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                media_type: 'movie',
-                media_id: movieId,
-                favorite: true,
-            }),
-        }
-    );
-
-    const data = await response.json();
-
-    return data;
 }
